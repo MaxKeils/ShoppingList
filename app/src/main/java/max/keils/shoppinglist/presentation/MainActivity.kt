c9ed69d9331043a9b3a42d1edee629fb1e3ed595
Maxim Martyanov <martyanovmax@mail.ru>
2024-01-02 20:28:40 +0300

    !

--- app/src/main/java/max/keils/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/max/keils/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,23 @@ package max.keils.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import max.keils.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivityDebug", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
