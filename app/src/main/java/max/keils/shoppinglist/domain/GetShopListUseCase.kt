203b397c608d85a57ec7414f9daf3b8eb5b41943
Maxim Martyanov <martyanovmax@mail.ru>
2024-01-11 19:29:51 +0300

    implementations recycler view

--- app/src/main/java/max/keils/shoppinglist/domain/GetShopListUseCase.kt
+++ app/src/main/java/max/keils/shoppinglist/domain/GetShopListUseCase.kt
@@ -1,8 +1,10 @@
 package max.keils.shoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 class GetShopListUseCase(private val repository: ShopListRepository) {
 
-    fun getShopList(): List<ShopItem> {
+    fun getShopList(): LiveData<List<ShopItem>> {
         return repository.getShopList()
     }
 
